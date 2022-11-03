67270901ae08dcbe97972184691a5e524ea10895
tenderking <gmu003@gmail.com>
2022-11-03 01:11:39 +0100

    fix: stable navbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -4,100 +4,91 @@
       <IconLogo />
     </NuxtLink>
 
-    <div>
-      <ul :class="{ hidden: isMobile === true && isOpen === false }">
-        <li>
-          <NuxtLink tabindex="0" to="/">Work</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink tabindex="0" to="/about">About</NuxtLink>
-        </li>
-        <!-- <li>
+
+    <ul ref="target" :class="isHidden ? 'hidden' : 'show'" v-on-click-outside="closeModal">
+      <li>
+        <NuxtLink tabindex="0" to="/">Work</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink tabindex="0" to="/about">About</NuxtLink>
+      </li>
+      <!-- <li>
           <NuxtLink tabindex="0" to="/Contact">Contact</NuxtLink>
         </li> -->
-        <!-- <li>
+      <!-- <li>
           <NuxtLink tabindex="0" to="/Blog">Blog</NuxtLink>
         </li> -->
-      </ul>
-    </div>
-    <i @click="toggleButton()" v-if="isMobile">
-      <IconClose v-if="isOpen" />
-      <IconHamburger v-else />
+    </ul>
+
+
+    <i v-if="isMobile">
+      <IconHamburger v-if="isHidden" @click="openModal" />
+      <IconClose v-else @click="closeModal" />
     </i>
   </nav>
 </template>
 <script lang="ts" setup>
-import { useMediaQuery, useToggle } from "@vueuse/core";
-const isMobile = useMediaQuery("(max-width: 640px)");
-// const target = ref(null)
-const [isOpen = ref(false), toggleButton] = useToggle();
+import { vOnClickOutside } from '@vueuse/components'
+const isHidden = ref(true)
+const isMobile = useMediaQuery("(max-width: 640px)")
 
-// function closeMenu() {
-//   isOpen.value = false
-// }
-// onClickOutside(target, () => {
-//   isOpen.value = false
-// })
-
-watch(isMobile, () => {
-  isOpen.value = false
-})
-</script>
-<style lang="scss">
-.hidden {
-  display: none;
+function closeModal() {
+  isHidden.value = true
 }
-
+function openModal() {
+  isHidden.value = false
+}
+</script>
+<style lang="scss" scoped>
 nav {
-  width: 100%;
+  padding: 1em;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 1rem;
-  background-color: var(--bg-element);
-  position: relative;
 
-  div {
+
+  ul {
     position: absolute;
+    inset: 2em 0 auto 0;
+    background-color: var(--bg);
+    gap: 1em;
+    flex-direction: column;
 
-    height: 100vh;
-    top: 1em;
-    right: 2rem;
-    left: 2rem;
+    padding: 1em;
+    gap: 1em;
+    border-radius: 1em;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
     text-align: center;
     z-index: 10;
-    margin-top: 2em;
-
-    ul {
-      padding: 1em;
-      gap: 1em;
-      border-radius: 1em;
-      box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
-      background-color: var(--bg-element);
-      display: flex;
-      flex-direction: column;
-    }
+    margin-inline: 2em;
   }
+
+  .hidden {
+    display: none;
+  }
+
+  .show {
+    display: flex;
+  }
+
+
 }
 
-@media (min-width: 640px) {
+@media (min-width:640px) {
   nav {
-    padding-inline: 16px;
-
-
-    div {
+    ul {
       position: static;
-      height: auto;
-      width: auto;
-      margin: 0;
+      inset: 2em 0 auto 0;
+      padding: 0;
+      display: flex;
+      gap: 1em;
+      flex-direction: row;
+      background-color: var(--bg);
 
-      ul {
-        margin: 0;
-        padding: 0;
-        flex-direction: row;
-        border-radius: 0;
-        box-shadow: none;
-      }
+    }
+
+    .hidden {
+      display: flex;
     }
   }
 }
